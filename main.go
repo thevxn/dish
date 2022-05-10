f32448fdd93fb51c114fae82a2ecddc564ecf4de
krusty <krusty@savla.dev>
2022-05-11 00:13:24 +0200

    runner: adding RawConnect method for fast socket testing + debugging log messages

--- main.go
+++ main.go
@@ -3,31 +3,53 @@ package main
 
 import (
 	"fmt"
+
 	"savla-dish/messenger"
 	"savla-dish/runner"
+	//"savla-dish/telnet"
 	"savla-dish/zasuvka"
 )
 
+const (
+	devMode = true
+	// could be a character/type byte too maybe
+	newLine string = "%0A"
+	socketsListFile string = "demo_sockets.json"
+)
+
 func main() {
-	endpointFile := "demo_sockets.json"
+	// load init config/socket list to run tests on
+	sockets := zasuvka.GibPole(socketsListFile, false)
 
-	sockets := zasuvka.GibPole(endpointFile, false)
-
-	newLine := "%0A"
+	// header
 	msgText := fmt.Sprintf("savla-dish run results: %s", newLine)
 
+	// iterate over given/loaded sockets
 	for i := 0; i < len(sockets.Sockets); i++ {
-		e := sockets.Sockets[i].Endpoint
+	//for _, sock := range sockets.Sockets {
+		//h := sock.Endpoint
+		//p := sock.Port
+		h := sockets.Sockets[i].Host
 		p := sockets.Sockets[i].Port
+		//e := net.JoinHostPort(h, p)
 
-		status := runner.CheckSite(e, p)
+		// testing paradigmas 
+		//status := runner.CheckSite(h, p)
+		//status := telnet.TestDial(h, p)
+		status, _ := runner.RawConnect(h, p)
 
-		msgText += fmt.Sprintf("%s %d %d %s", e, p, status, newLine)
+		msgText += fmt.Sprintf("%s %d %d %s", h, p, status, newLine)
 
-		fmt.Println(e, p, status)
+		fmt.Println(h, p, status)
 	}
 
-	messenger.SendMsg(msgText)
+	// mute dish messenger if needed in a custom build/env
+	if !devMode {
+		messenger.SendMsg(msgText)
+	}
+
+	// final report output to stdout/console/logs
+	fmt.Printf(msgText)
 
 	//fmt.Println( resp.Status )
 	//fmt.Println( resp.StatusCode )
--- runner/runner.go
+++ runner/runner.go
@@ -1,25 +1,73 @@
 package runner
 
 import (
-	/*"fmt"*/
 	"log"
+	"net"
 	"net/http"
+	"strconv"
+	//"time"
 )
 
+
+func RawConnect(host string, port int) (status int, err error) {
+	// reformat int port to number string
+	portString := strconv.Itoa(port)
+
+	// vars
+	endpoint := net.JoinHostPort(host, portString)
+	//endpoint := host + ":" + portString
+	//protocol := "tcp"
+	//timeout := time.Second
+
+	log.Println("runner: " + endpoint)
+
+	// open the socket
+	//conn, err := net.DialTimeout("tcp", endpoint, timeout)
+	conn, err := net.Dial("tcp", endpoint)
+
+	// Close open conn after 5 seconds
+	//&conn.SetReadDeadline(time.Seconds(&timeout))
+
+	// prolly more possible to get not-nil err, than not-nil conn 
+	// https://stackoverflow.com/a/56336811
+	if err != nil {
+		log.Println("runner: conn error: " + endpoint)
+		log.Println(err)
+		return 1, err
+	}
+	if conn != nil {
+		defer conn.Close()
+		//log.Println("[-] Opened: " + conn.Read())
+		return 0, nil
+	}
+
+	return 0, nil
+}
+
 func CheckSite(endpoint string, port int) (status int) {
+	var netClient = &http.Client{}
+
+	resp, err := netClient.Get("http://" + endpoint + "" + strconv.Itoa(port))
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	log.Print(resp)
+	return 0
+
+	/*
 	req, err := http.NewRequest("GET", endpoint, nil)
 
 	if err != nil {
-		// status = err
-		// continue
-		log.Fatal(err)
+		log.Fatalln("lmao")
 	}
 
 	req.Header.Set("Content-Type", "application/json")
 	resp, err := http.DefaultClient.Do(req)
 
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalln("lol")
 	}
 
 	defer resp.Body.Close()
@@ -35,5 +83,5 @@ func CheckSite(endpoint string, port int) (status int) {
 			fmt.Println(resp.Status)
 	}*/
 
-	return resp.StatusCode
+	//return resp.StatusCode
 }
