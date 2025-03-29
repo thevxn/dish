b370f0601acf3a18b9bc3759321f29062c418203
Tackx <tack@vxn.dev>
2025-03-29 13:35:05 +0100

    Update -h flag text

--- cmd/dish/main.go
+++ cmd/dish/main.go
@@ -17,7 +17,7 @@ func printHelp() {
 	fmt.Print("Usage: dish [FLAGS] SOURCE\n\n")
 	fmt.Print("A lightweight, one-shot socket checker\n\n")
 	fmt.Println("SOURCE must be a file path leading to a JSON file with a list of sockets to be checked or a URL leading to a remote JSON API from which the list of sockets can be retrieved")
-	fmt.Println("Use `dish -h` for a list of flags")
+	fmt.Println("Use the `-h` flag for a list of available flags")
 }
 
 // fanInChannels collects results from multiple goroutines
