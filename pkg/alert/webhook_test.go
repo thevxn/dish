5e51abea19a71cce3634d82562f241ef90173adc
Tackx <tack@vxn.dev>
2025-04-06 16:13:10 +0200

    Add webhook tests

---
+++ pkg/alert/webhook_test.go
@@ -0,0 +1,165 @@
+package alert
+
+import (
+	"reflect"
+	"testing"
+
+	"go.vxn.dev/dish/pkg/testhelpers"
+)
+
+func TestNewWebhookSender(t *testing.T) {
+	mockHTTPClient := &testhelpers.SuccessStatusHTTPClient{}
+
+	url := "https://abc123.xyz.com"
+	notifySuccess := false
+	verbose := false
+
+	expected := &webhookSender{
+		httpClient:    mockHTTPClient,
+		url:           url,
+		notifySuccess: notifySuccess,
+		verbose:       verbose,
+	}
+
+	actual, _ := NewWebhookSender(mockHTTPClient, url, verbose, notifySuccess)
+
+	if !reflect.DeepEqual(expected, actual) {
+		t.Fatalf("expected %v, got %v", expected, actual)
+	}
+}
+
+func TestSend_Webhook(t *testing.T) {
+	url := "https://abc123.xyz.com"
+
+	successResults := Results{
+		Map: map[string]bool{
+			"test": true,
+		},
+	}
+	failedResults := Results{
+		Map: map[string]bool{
+			"test": false,
+		},
+	}
+	mixedResults := Results{
+		Map: map[string]bool{
+			"test1": true,
+			"test2": false,
+		},
+	}
+
+	tests := []struct {
+		name          string
+		client        HTTPClient
+		results       Results
+		failedCount   int
+		notifySuccess bool
+		verbose       bool
+		wantErr       bool
+	}{
+		{
+			name:          "Failed Sockets",
+			client:        &testhelpers.SuccessStatusHTTPClient{},
+			results:       failedResults,
+			failedCount:   1,
+			notifySuccess: false,
+			verbose:       false,
+			wantErr:       false,
+		},
+		{
+			name:          "Failed Sockets - Verbose",
+			client:        &testhelpers.SuccessStatusHTTPClient{},
+			results:       failedResults,
+			failedCount:   1,
+			notifySuccess: false,
+			verbose:       true,
+			wantErr:       false,
+		},
+		{
+			name:          "No Failed Sockets With notifySuccess",
+			client:        &testhelpers.SuccessStatusHTTPClient{},
+			results:       successResults,
+			failedCount:   0,
+			notifySuccess: true,
+			verbose:       false,
+			wantErr:       false,
+		},
+		{
+			name:          "No Failed Sockets Without notifySuccess",
+			client:        &testhelpers.SuccessStatusHTTPClient{},
+			results:       successResults,
+			failedCount:   0,
+			notifySuccess: false,
+			verbose:       false,
+			wantErr:       false,
+		},
+		{
+			name:          "No Failed Sockets Without notifySuccess - Verbose",
+			client:        &testhelpers.SuccessStatusHTTPClient{},
+			results:       successResults,
+			failedCount:   0,
+			notifySuccess: false,
+			verbose:       true,
+			wantErr:       false,
+		},
+		{
+			name:          "Mixed Results With notifySuccess",
+			client:        &testhelpers.SuccessStatusHTTPClient{},
+			results:       mixedResults,
+			failedCount:   1,
+			notifySuccess: true,
+			verbose:       false,
+			wantErr:       false,
+		},
+		{
+			name:          "Mixed Results Without notifySuccess",
+			client:        &testhelpers.SuccessStatusHTTPClient{},
+			results:       mixedResults,
+			failedCount:   1,
+			notifySuccess: false,
+			verbose:       false,
+			wantErr:       false,
+		},
+		{
+			name:          "Network Error When Pushing to Webhook",
+			client:        &testhelpers.FailureHTTPClient{},
+			results:       failedResults,
+			failedCount:   1,
+			notifySuccess: false,
+			verbose:       false,
+			wantErr:       true,
+		},
+		{
+			name:          "Unexpected Response Code From Webhook",
+			client:        &testhelpers.ErrorStatusHTTPClient{},
+			results:       failedResults,
+			failedCount:   1,
+			notifySuccess: false,
+			verbose:       false,
+			wantErr:       true,
+		},
+		{
+			name:          "Error Reading Response Body From Webhook",
+			client:        &testhelpers.InvalidResponseBodyHTTPClient{},
+			results:       failedResults,
+			failedCount:   1,
+			notifySuccess: false,
+			verbose:       true,
+			wantErr:       true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			sender, err := NewWebhookSender(tt.client, url, tt.verbose, tt.notifySuccess)
+			if err != nil {
+				t.Fatalf("failed to create Webhook sender instance: %v", err)
+			}
+
+			err = sender.send(tt.results, tt.failedCount)
+			if tt.wantErr != (err != nil) {
+				t.Errorf("expected error: %v, got: %v", tt.wantErr, err)
+			}
+		})
+	}
+}
