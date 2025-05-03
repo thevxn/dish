1085e46b136fa8dbe6b0750a9958fbb27319c522
Konstanty Cyran <96084033+krixlion@users.noreply.github.com>
2025-05-03 16:09:05 +0200

    test(netrunner): add 10s timeout to icmp test

--- pkg/netrunner/runner_test.go
+++ pkg/netrunner/runner_test.go
@@ -376,7 +376,10 @@ func TestIcmpRunner_RunTest(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got := tt.runner.RunTest(context.Background(), tt.args.sock)
+			ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+			defer cancel()
+
+			got := tt.runner.RunTest(ctx, tt.args.sock)
 			if !cmp.Equal(got, tt.want, cmpopts.EquateErrors()) {
 				t.Fatalf("icmpRunner.RunTest():\n got = %v\n want = %v", got, tt.want)
 			}
