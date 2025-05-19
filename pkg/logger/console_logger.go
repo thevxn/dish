0c642f7b1401555d9fc3effc55a4b552227ffc46
Tackx <tack@vxn.dev>
2025-05-19 18:48:12 +0200

    Make consoleLogger unexported, Minor grammar fixes

--- pkg/logger/console_logger.go
+++ pkg/logger/console_logger.go
@@ -6,16 +6,16 @@ import (
 	"os"
 )
 
-// ConsoleLogger logs output to stderr.
-type ConsoleLogger struct {
+// consoleLogger logs output to stderr.
+type consoleLogger struct {
 	stdLogger *log.Logger
 	logLevel  LogLevel
 }
 
 // NewConsoleLogger creates a new ConsoleLogger instance,
 // If verbose is true, log level is set to TRACE (otherwise to INFO).
-func NewConsoleLogger(verbose bool) *ConsoleLogger {
-	l := &ConsoleLogger{
+func NewConsoleLogger(verbose bool) *consoleLogger {
+	l := &consoleLogger{
 		stdLogger: log.New(os.Stderr, "", log.LstdFlags),
 	}
 
@@ -27,9 +27,9 @@ func NewConsoleLogger(verbose bool) *ConsoleLogger {
 	return l
 }
 
-// log prints a message if the current log level allows it,
-// It adds passed prefix and formats output if a format string is passed.
-func (l *ConsoleLogger) log(level LogLevel, prefix string, format string, v ...any) {
+// log prints a message if the current log level allows it.
+// It adds the passed prefix and formats the output if a format string is passed.
+func (l *consoleLogger) log(level LogLevel, prefix string, format string, v ...any) {
 	if l.logLevel > level {
 		return
 	}
@@ -46,50 +46,50 @@ func (l *ConsoleLogger) log(level LogLevel, prefix string, format string, v ...a
 	}
 }
 
-func (l *ConsoleLogger) Trace(v ...any) {
+func (l *consoleLogger) Trace(v ...any) {
 	l.log(TRACE, "TRACE:", "", v...)
 }
 
-func (l *ConsoleLogger) Tracef(f string, v ...any) {
+func (l *consoleLogger) Tracef(f string, v ...any) {
 	l.log(TRACE, "TRACE:", f, v...)
 }
 
-func (l *ConsoleLogger) Debug(v ...any) {
+func (l *consoleLogger) Debug(v ...any) {
 	l.log(DEBUG, "DEBUG:", "", v...)
 }
 
-func (l *ConsoleLogger) Debugf(f string, v ...any) {
+func (l *consoleLogger) Debugf(f string, v ...any) {
 	l.log(DEBUG, "DEBUG:", f, v...)
 }
 
-func (l *ConsoleLogger) Info(v ...any) {
+func (l *consoleLogger) Info(v ...any) {
 	l.log(INFO, "INFO:", "", v...)
 }
 
-func (l *ConsoleLogger) Infof(f string, v ...any) {
+func (l *consoleLogger) Infof(f string, v ...any) {
 	l.log(INFO, "INFO:", f, v...)
 }
 
-func (l *ConsoleLogger) Warn(v ...any) {
+func (l *consoleLogger) Warn(v ...any) {
 	l.log(WARN, "WARN:", "", v...)
 }
 
-func (l *ConsoleLogger) Warnf(f string, v ...any) {
+func (l *consoleLogger) Warnf(f string, v ...any) {
 	l.log(WARN, "WARN:", f, v...)
 }
 
-func (l *ConsoleLogger) Error(v ...any) {
+func (l *consoleLogger) Error(v ...any) {
 	l.log(ERROR, "ERROR:", "", v...)
 }
 
-func (l *ConsoleLogger) Errorf(f string, v ...any) {
+func (l *consoleLogger) Errorf(f string, v ...any) {
 	l.log(ERROR, "ERROR:", f, v...)
 }
 
-func (l *ConsoleLogger) Panic(v ...any) {
+func (l *consoleLogger) Panic(v ...any) {
 	l.log(PANIC, "PANIC:", "", v...)
 }
 
-func (l *ConsoleLogger) Panicf(f string, v ...any) {
+func (l *consoleLogger) Panicf(f string, v ...any) {
 	l.log(PANIC, "PANIC:", f, v...)
 }
