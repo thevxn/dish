3013c354712ad332417adbcb89f94efa051352f2
Mihai Tanase <mihai.tanase@sap.com>
2025-06-28 10:23:16 +0200

    feat: add colored log level

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -13,6 +13,45 @@ const (
 	PANIC
 )
 
+var logColors = map[logLevel]string{
+	TRACE: "\033[34m", // Blue
+	DEBUG: "\033[36m", // Cyan
+	INFO:  "\033[32m", // Green
+	WARN:  "\033[33m", // Yellow
+	ERROR: "\033[31m", // Red
+	PANIC: "\033[35m", // Magenta
+}
+
+var logPrefixes = map[logLevel]string{
+	TRACE: "[ TRACE ]: ",
+	DEBUG: "[ DEBUG ]: ",
+	INFO:  "[ INFO ]: ",
+	WARN:  "[ WARN ]: ",
+	ERROR: "[ ERROR ]: ",
+	PANIC: "[ PANIC ]: ",
+}
+
+func (l logLevel) Color() string {
+	if color, exists := logColors[l]; exists {
+		return color
+	}
+	return "\033[0m" // Default color (reset)
+}
+
+func (l logLevel) Prefix(withColor bool) string {
+	prefix, prefixExists := logPrefixes[l]
+
+	if !prefixExists {
+		return "[ UNKNOWN ]: "
+	}
+
+	if withColor {
+		return l.Color() + prefix + "\033[0m"
+	}
+
+	return prefix
+}
+
 // Logger interface defines methods for logging at various levels.
 type Logger interface {
 	Trace(v ...any)
